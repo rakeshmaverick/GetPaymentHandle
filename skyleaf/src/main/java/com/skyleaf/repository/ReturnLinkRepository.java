eb003fad1a6e4b522fe41e5d302fe78c27086bd1
Rakesh Y <78504277+rakeshmaverick@users.noreply.github.com>
2021-03-11 12:51:33 +0530

    Get Payment Handle program

---
+++ skyleaf/src/main/java/com/skyleaf/repository/ReturnLinkRepository.java
@@ -0,0 +1,9 @@
+package com.skyleaf.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.skyleaf.beans.ReturnLinks;
+
+public interface ReturnLinkRepository extends CrudRepository<ReturnLinks, String>{
+
+}
